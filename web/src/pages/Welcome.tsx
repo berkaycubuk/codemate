82ac671a509f2953d7c4a2ef6f82641ea948b416
Berkay Çubuk <berkaycubuk@gmail.com>
2021-02-27 22:43:43 +0300

    hotfix as usual

--- web/src/pages/Welcome.tsx
+++ web/src/pages/Welcome.tsx
@@ -35,7 +35,7 @@ export default function Home() {
     <div className="my-4">
       <h1 className="text-2xl font-bold mb-2">Codemate</h1>
       <p className="text-lg">Meet with new people who likes your favorite programming language.</p>
-      <a className="inline-block mt-4 px-3 py-2 text-lg bg-black text-white hover:opacity-80" href="https://github.com/login/oauth/authorize?client_id=874c8b8f25b7277b9b30&redirect_uri=http://localhost:8000/auth/github/callback/web">Login with Github</a>
+      <a className="inline-block mt-4 px-3 py-2 text-lg bg-black text-white hover:opacity-80" href="https://github.com/login/oauth/authorize?client_id=874c8b8f25b7277b9b30&redirect_uri=https://codemate-api.herokuapp.com/auth/github/callback/web">Login with Github</a>
     </div>
   )
 }
